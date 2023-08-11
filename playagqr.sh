6a1532065d46e86f8e122e6559d37a4dab97ebac
diff <diff@plaintextfiles.org>
2023-08-11 16:06:07 +0900

    Create playagqr.sh

---
+++ playagqr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#ffplay -nodisp "https://www.uniqueradio.jp/agplayer5/hls/mbr-1-cdn.m3u8"
+#ffplay -nodisp "https://hlsb1.cdnext.stream.ne.jp/agqr1/iphone/3Gs.m3u8"
+ffplay -nodisp "https://www.uniqueradio.jp/agapps/hls/cdn.m3u8"
